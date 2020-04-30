008ff319c67262ee62f465c60ec1616974af39ce
srujan-gudla <59172267+srujan-gudla@users.noreply.github.com>
2020-04-30 11:53:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var backgroundImage, database;
+var position,form,player,game;
+var gameState=0;
+var playerCount;
+var allPlayers;
+function setup(){
+  database = firebase.database();
+  createCanvas(500,500);
+  game=new Game;
+  game.getState();
+  game.start()
+}
+
+function draw(){
+  if (playerCount===4){
+    game.update(1)
+  }
+  if(gameState===1){
+    clear();
+    game.play();
+  }
+}
